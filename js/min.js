5268f2800b6bef3d378894d4b009575e511088d9
Oleh <opolonetso@gmail.com>
2022-02-17 19:36:00 +0200

    add slider

---
+++ js/min.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    loop: true,
+    speed: 400,
+    spaceBetween: 70,
+    centeredSlides: true,
+    slideToClickedSlide: true,
+    toggle: true,
+    /* breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        }
+    } */
+});
