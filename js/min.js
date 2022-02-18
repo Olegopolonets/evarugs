52ed1e16298023a12fe692c052e222df8aad4921
Oleh <opolonetso@gmail.com>
2022-02-18 20:45:51 +0200

    add modal

--- js/min.js
+++ js/min.js
@@ -15,5 +15,19 @@ const swiper = new Swiper('.swiper', {
             slidesPerView: 2,
             spaceBetween: 40
         }
-    } */
+    } 
+      */
 });
+
+const modalWindow = document.querySelector('.modal');
+const buttonModal = document.querySelector('.main__display-button');
+
+buttonModal.addEventListener('click', () => {
+    modalWindow.classList.add('active');
+})
+
+modalWindow.addEventListener('click', (e) => {
+    const isModal = e.target.closest('.modal__inner');
+
+    modalWindow.classList.remove('active');
+})
